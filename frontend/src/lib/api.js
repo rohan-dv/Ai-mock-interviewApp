444659f469c04af2c8031d650c631a384be2115b
Veer <v70132877@gmail.com>
2026-06-18 16:06:54 +0000

    Add frontend api, auth, and utility helpers

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+export const API = `${BACKEND_URL}/api`;
+
+export const api = axios.create({ baseURL: API });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      const path = window.location.pathname;
+      if (!["/login", "/signup", "/"].includes(path)) {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(err);
+  }
+);
